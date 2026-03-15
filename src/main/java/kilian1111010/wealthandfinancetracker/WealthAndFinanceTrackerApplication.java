b9bc52cb81df027c9e6aa48f7a61a936bc24f309
Kilian1111010 <175934284+Kilian1111010@users.noreply.github.com>
2026-03-15 14:21:48 +0100

    first commit

---
+++ src/main/java/kilian1111010/wealthandfinancetracker/WealthAndFinanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package kilian1111010.wealthandfinancetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthAndFinanceTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WealthAndFinanceTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kilian1111010/wealthandfinancetracker/WealthAndFinanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package kilian1111010.wealthandfinancetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WealthAndFinanceTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
